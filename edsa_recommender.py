a69f02c60a7e1485099c48a9d1088c62a1154749
Pheelog <93024326+Pheelog@users.noreply.github.com>
2022-07-14 03:07:16 +0100

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -106,10 +106,53 @@ def main():
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
 	
-    if page_selection == "About Us":
-        st.title("About Us")
-        st.write("Polaroid is an institution that makes sense of data using machine learning methods to \
-		  provide solutions that improve lives. Our mission is to drive changes that lead to a greener earth.")    
+st.markdown("<div class='about-head'> About Us </div>", unsafe_allow_html=True)
+
+st.markdown('''<p class='about-body'> Polaroid is an institution that makes sense of data using machine learning methods
+                                       to provide solutions that improve lives. Our mission is to drive changes that lead to a greener earth.
+                                       </p>''', unsafe_allow_html=True)
+
+st.markdown(
+    f"""
+        <div class="each-image">
+            <div class="img-cont">
+                <img class="team-img" src="data:image/png;base64,{base64.b64encode(open('resources/imgs/pheelo.jpeg', "rb").read()).decode()}">
+            </div>
+            <p class="team-name">Philip Ogunmola</p>
+        </div>
+        <div class="each-image">
+            <div class="img-cont">
+                <img class="team-img" src="data:image/png;base64,{base64.b64encode(open('resources/imgs/abdul.jpeg', "rb").read()).decode()}">
+            </div>
+            <p class="team-name">Abubakar Abdulkadir</p>
+        </div>
+        <div class="each-image">
+            <div class='img-cont'>
+                <img class="team-img" src="data:image/png;base64,{base64.b64encode(open('resources/imgs/joseph.jpeg', "rb").read()).decode()}">
+            </div>
+            <p class="team-name">Joseph Mugo</p>
+        </div>
+        <div class="each-image">
+            <div class='img-cont'>
+                <img class="team-img" src="data:image/png;base64,{base64.b64encode(open('resources/imgs/rogers.jpeg', "rb").read()).decode()}">
+            </div>
+            <p class="team-name">Rogers Mugambi</p>
+        </div>
+        <div class="each-image">
+            <div class='img-cont'>
+                <img class="team-img" src="data:image/png;base64,{base64.b64encode(open('resources/imgs/nana.jpeg', "rb").read()).decode()}" />
+            </div>
+            <p class="team-name">Adewale Nana</p>
+        </div>
+        <div class="each-image">
+            <div class='img-cont'>
+                <img class="team-img" src="data:image/png;base64,{base64.b64encode(open('resources/imgs/philip.jpeg', "rb").read()).decode()}">
+            </div>
+            <p class="team-name">Philip Wambua</p>
+        </div>
+    """,
+    unsafe_allow_html=True
+)  
 	
 
 
